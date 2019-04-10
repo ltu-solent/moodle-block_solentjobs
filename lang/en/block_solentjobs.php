462a155525fd266a988e5d6b2e724bffd372fefe
Sarah Cotton <sarah.cotton@solent.ac.uk>
2019-04-10 07:57:07 +0100

    Block name updated

--- lang/en/block_solentjobs.php
+++ lang/en/block_solentjobs.php
@@ -1,6 +1,6 @@
 <?php
 $string['pluginname'] = 'Solent Jobs';
-$string['solentjobs'] = 'Solent Jobs';
+$string['solentjobs'] = 'Jobs added by Solent University';
 $string['textfields'] = 'Solent Jobs';
 $string['solentjobs:addinstance'] = 'Add a new Solent Jobs block';
 $string['solentjobs:myaddinstance'] = 'Add a new Solent Jobs block to the My Moodle page';
